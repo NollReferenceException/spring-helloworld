7dba11a951696cb57a88310bdd050abd12e3737a
NollReferenceException <itema453@gmail.com>
2024-03-10 07:41:40 +0400

    solved

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorldBeanFirst =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorldBeanSecond =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBeanFirst =
+                (Cat) applicationContext.getBean("cat");
+        Cat catBeanSecond =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(helloWorldBeanFirst == helloWorldBeanSecond);
+        System.out.println(catBeanFirst == catBeanSecond);
     }
 }
